bb6e0aa346ebe813da3f52040d69e0f9c2bb3ec7
sophia <sophiadenisovichgovtva@gmail.com>
2020-12-09 18:26:39 +0200

    Added name and city

---
+++ src/main/java/InputOutputTest.java
@@ -0,0 +1,13 @@
+package main.java;
+import java.util.Scanner;
+
+public class InputOutputTest {
+    public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      System.out.print("Please, input your name ");
+      String name = in.nextLine();
+      System.out.print("Please, input your city ");
+      String city = in.nextLine();
+      
+    }
+}
