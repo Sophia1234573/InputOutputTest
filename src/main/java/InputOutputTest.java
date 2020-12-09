0533597c42b55682d257b352f040029d165b5060
sophia <sophiadenisovichgovtva@gmail.com>
2020-12-09 18:34:53 +0200

    Added age and hobbies

--- src/main/java/InputOutputTest.java
+++ src/main/java/InputOutputTest.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 public class InputOutputTest {
     public static void main(String[] args) {
       Scanner in = new Scanner(System.in);
+
       System.out.print("Please, input your name ");
       String name = in.nextLine();
+
       System.out.print("Please, input your city ");
       String city = in.nextLine();
-      
+
+      System.out.print("Please, input your age ");
+      int age = in.nextInt();
+
+      System.out.print("Please, input your hobbies ");
+      String hobbies = in.nextLine();
     }
 }
