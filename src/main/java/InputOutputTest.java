db04ab60aaeee33bb41d46df0fd757977cd6d92e
sophia <sophiadenisovichgovtva@gmail.com>
2020-12-10 10:05:15 +0200

    program is done

--- src/main/java/InputOutputTest.java
+++ src/main/java/InputOutputTest.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class InputOutputTest {
     public static void main(String[] args) {
+
       Scanner in = new Scanner(System.in);
 
       System.out.print("Please, input your name ");
@@ -11,24 +12,28 @@ public class InputOutputTest {
       System.out.print("Please, input your city ");
       String city = in.nextLine();
 
+      System.out.print("Please, input your hobby ");
+      String hobby = in.nextLine();
+
       System.out.print("Please, input your age ");
       int age = in.nextInt();
 
-      System.out.print("Please, input your hobbies ");
-      String hobbies = in.nextLine();
-
+      System.out.println();
       System.out.println("Имя: " + name);
       System.out.println("Город: " + city);
       System.out.println("Возраст: " + age);
-      System.out.println("Хобби: " + hobbies);
+      System.out.println("Хобби: " + hobby);
 
-      System.out.println("Человек по имени " + name + " живет в городе " + city + ". Этому человеку лет " + age + " и любит он заниматься " + hobbies + ".");
+      System.out.println();
+      System.out.println("Человек по имени " + name + " живет в городе " + city + ". Этому человеку лет " + age + " и любит он заниматься " + hobby + ".");
 
+      System.out.println();
       System.out.println(name + " - имя");
       System.out.println(city + " - город");
       System.out.println(age + " - возраст");
-      System.out.println(hobbies + " - хобби");
+      System.out.println(hobby + " - хобби");
 
       in.close();
+
     }
 }
